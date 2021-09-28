84795d9f8651208cd7d0bd0bf6810bd8f92e7f16
1Mirl <miredi.lunga@colombbus.org>
2021-09-28 13:07:12 +0200

    maj JS
    Suite cours

---
+++ 02-variables-constantes/affichage.js
@@ -0,0 +1,25 @@
+//js pour la page affichage
+
+let ville = 'Nice';
+console.log(ville);
+
+let prixLogement = 1200;
+console.log(prixLogement);
+
+alert(prixLogement);
+
+alert('Bienvenue chez les Ch\'tis!')
+
+ //min exo ; demandez à un utilisateur sa ville de naissance et afficher le résultat dans une alert()
+
+prompt('votre age?');
+
+let villeNaissance;
+
+villeNaissance = prompt('Bonjour, Quelle est votre ville de naissance?');
+
+console.log(villeNaissance);
+
+alert(villeNaissance);
+
+alert('vous êtes né à '+ villeNaissance);
