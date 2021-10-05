7afc90259b5c9d0fb4e4ba45c5397ab1eeed0145
1Mirl <miredi.lunga@colombbus.org>
2021-10-05 17:06:17 +0200

    maj JS
    Suite cours JS

---
+++ 03_structures contrôle/if.js
@@ -0,0 +1,29 @@
+//fichier introduction.html des structures de contrôle
+
+//alert('coucou');
+
+let x = 4;
+let y = 0;
+//1er if
+if(x > 1){ // if (TEST) si test est évolué à true
+    alert('x contient ' + x + ' et cette valeur est supérieur à 1');
+    //on éxecute ce bloc de code
+}
+//2e if
+if(y < 1){
+    alert('y contient ' + y + ' et cette valeur est inférieur à 1');
+}
+//3e if
+if(x == y){
+    alert('x contient ' + x + ' ,et y contient ' + y + ' les deux variables contiennent la même valeur');
+}
+//4e if
+//l'alert ne s'affiche pas car la valeur de y qui contient 0, 0 est une des valeurs qui renvoie false
+if(y){
+    alert('la valeur de y est évaluée à "true"');
+}
+//5e if
+let chaine = '';
+if(NaN){
+    alert('5/ true /// vrai');
+}
--- 03_structures contrôle/introduction.js
+++ 03_structures contrôle/introduction.js
@@ -1,10 +1,34 @@
 //fichier introduction.html des structures de contrôle
 
 //alert('coucou');
-
+//test 1
 let x = '4';
 console.log(x);
 console.log(typeof x);
+console.log(typeof x, x); //il donne le type d'x et la valeur d'x
 
 let testEgalValeur = x == 4;
 console.log(testEgalValeur);
+
+//test 2
+let testEgalValeurType = x === 4;
+//le contenu égal en type de données ET valeur
+console.log(testEgalValeurType);//le résultat est faux car il n'est pas un number mais un STRING(ligne 5 parce qu'il y a des quotes).
+
+let testDifferenceValeur = x != 4;
+console.log(testDifferenceValeur);
+
+let testDifferenceValeurType = x !== 4; //différent en valeur OU en types
+console.log(testDifferenceValeurType);
+
+let testInferieur = x < 4;
+console.log(testInferieur);
+
+let testSuperieur = x > 4;
+console.log(testSuperieur);
+
+let testInferieurEgal = x <= 4;
+console.log(testInferieurEgal);
+
+let testSuperieurEgal = x <= 4;
+console.log(testSuperieurEgal);
