93f39af0933f5290e6c46dcf304c4a085abce804
1Mirl <miredi.lunga@colombbus.org>
2021-10-05 11:39:30 +0200

    00 EXO
    maj 01_bonjour et css et JS

---
+++ 00 EXO/bonjour.js
@@ -0,0 +1,8 @@
+let firstName = prompt('Entrez votre prénom');
+console.log(firstName);
+
+
+let lastName = prompt('Entrez votre nom');
+console.log(lastName);
+
+alert('Bonjour, '+ firstName +' '+ lastName +' !');
