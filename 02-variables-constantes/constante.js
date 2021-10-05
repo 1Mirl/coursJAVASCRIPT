be9f0015039cb924726a3a6a5aeadec3841cde9b
1Mirl <miredi.lunga@colombbus.org>
2021-10-05 13:08:36 +0200

    maj JS
    Suite JS

---
+++ 02-variables-constantes/constante.js
@@ -0,0 +1,22 @@
+//fichier pour la page constantes
+
+//alert('coucou');
+
+let prenom = 'André';
+console.log(prenom);
+
+prenom = 'oui, c\'est bon ! Arrêtez je ne suis pas idiot!';
+console.log(prenom);
+
+prenom = 'HIHI';
+console.log(prenom);
+
+const nom = 'Duras';
+console.log(nom);
+
+const age = '800';
+console.log(age);
+
+//Uncought TypeError: invalid assignment to const 'nom'
+//nom = 'Youcenar';
+//INTERDICTION DE RE AFFECTER UNE CONSTANTE
---
+++ 03_structures contrôle/introduction.js
@@ -0,0 +1,10 @@
+//fichier introduction.html des structures de contrôle
+
+//alert('coucou');
+
+let x = '4';
+console.log(x);
+console.log(typeof x);
+
+let testEgalValeur = x == 4;
+console.log(testEgalValeur);
