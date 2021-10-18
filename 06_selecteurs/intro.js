197680110298f54c40251868a3e263b775fedfc4
1Mirl <miredi.lunga@colombbus.org>
2021-10-18 15:48:36 +0200

    maj cours JS
    Suite cours JS

--- 06_selecteurs/intro.js
+++ 06_selecteurs/intro.js
@@ -1,6 +1,6 @@
 //fichier 06_01_introduction.html 
 
-alert('coucou');
+//alert('coucou');
 
 //avec cet événement window.onload toute la page est chargée et cela permet de mettre le script dans head sans async ou defer
 window.onload = function() {
